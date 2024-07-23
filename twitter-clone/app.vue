00ebe96ab370d1f59895c9f8c2649adad14cad60
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-07-23 14:49:30 +0300

    feat: Start right sidebar

--- twitter-clone/app.vue
+++ twitter-clone/app.vue
@@ -1,33 +1,40 @@
 <template>
   <div :class="{'dark': darkMode}">
     <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
 
-    <div class="min-h-full">
 
-      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
-
-        <!-- Left sidebar -->
-
-        <div class="hidden md:block xs-col-span-1 xl-col-span-2">
-          <div class="sticky top-0">
-
+          <!-- Left sidebar -->
+          <div class="hidden md:block md:col-span-2 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft/>
+            </div>
           </div>
-          <sidebar-left/>
+
+
+          <!-- Main content -->
+          <main class="col-span-12 bg-red-500 md:col-span-8 xl:col-span-6">
+            <h1>Main content</h1>
+          </main>
+
+
+          <!-- Right sidebar -->
+          <div class="hidden col-span-12  md:block md:col-span-3 xl:col-span-4">
+            <div class="sticky top-0">
+              <SidebarRight/>
+            </div>
+          </div>
+
 
         </div>
-
-        <!-- Main content -->
-
-        <!-- Right sidebar -->
-
       </div>
-
     </div>
-
-  </div>
-
   </div>
 </template>
+
 <script setup>
-const darkMode = ref(false)
+import { ref } from 'vue';
+
+const darkMode = ref(false);
 </script>
